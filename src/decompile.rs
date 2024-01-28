33a7dcc62fed754efd4a0df3f6d5a95bfbda293c
GalileoCap <galileocapp@gmail.com>
2024-01-28 06:19:46 -0300

    OK: Decompile test macro

--- src/decompile.rs
+++ src/decompile.rs
@@ -2,9 +2,61 @@ use smpl_core_common::Instruction;
 use crate::{VM, utils::{Error, Result}};
 
 pub fn decompile(vm : &VM, addr : u16) -> (Result<Instruction>, u16) {
-    match vm.get_mem(addr) {
-        0x00 => (Ok(Instruction::Nop), 2),
+    let inst = match vm.get_mem(addr) {
+        0x00 => Ok(Instruction::Nop),
 
-        opcode => (Err(Error::InvalidOpcode(opcode, vm.get_mem(addr + 1))), 2),
+        opcode => Err(Error::InvalidOpcode(opcode, vm.get_mem(addr + 1))),
+    };
+
+    let len = inst.as_ref().map_or(2, |inst| inst.len() as u16);
+    (inst, len)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! case_gen {
+        ($ident:ident, $code:literal, $vm:ident, $res:ident, $expect:ident, $assert:block) => {
+            #[test]
+            fn $ident() {
+                let ram = sasm_lib::compile($code).unwrap();
+                let mut $vm = VM::new(ram);
+                $vm.set_reg(&smpl_core_common::Register::RIP, 0x0000);
+
+                let $expect = sasm_lib::parse($code).unwrap()[0];
+                let $res = $vm.decompile_next();
+
+                $assert
+            }
+        };
     }
+
+    macro_rules! case {
+        ($ident:ident, $code:literal) => {
+            case_gen!($ident, $code, vm, res, expect, { 
+                assert_eq!(res, Ok(expect));
+                assert_eq!(*vm.get_reg(&smpl_core_common::Register::RIP), expect.len() as u16);
+            });
+        };
+    }
+
+    case!(nop, "nop");
+    case_gen!(db, "db 0xF3, 0x37", vm, res, _expect, {
+        assert_eq!(res, Err(Error::InvalidOpcode(0xF3, 0x37)));
+    });
+
+    case!(movc2r_byte, "mov 0xF3, rb0");
+    case!(movc2r_word, "mov 0xF337, r1");
+    case!(movr2r_byte, "mov rb2, rb3");
+    case!(movr2r_word, "mov r4, r5");
+    case!(movm2r, "mov [r6], rb7");
+    case!(movr2m, "mov rb8, [r9]");
+
+    case!(add_byte, "add rb10, rb11");
+    case!(add_word, "add r0, r1");
+    case!(sub_byte, "sub r0, r1");
+    case!(sub_word, "sub r0, r1");
+
+    case!(jmp, "jmp r0");
 }
